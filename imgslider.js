dd958d17e083654c559c3c6e4a537e896c16785a
VU21CSEN0101935CHARANTEJ <104959928+VU21CSEN0101935CHARANTEJ@users.noreply.github.com>
2024-08-31 20:16:11 +0530

    Create imgslider.js

---
+++ imgslider.js
@@ -0,0 +1,36 @@
+ const slides = document.querySelector('.slides');
+    const slide = document.querySelectorAll('.slide');
+    const prevButton = document.querySelector('.prev');
+    const nextButton = document.querySelector('.next');
+    let index = 0;
+    let autoSlide;
+
+    function showSlide(i) {
+        index = (i + slide.length) % slide.length;
+        slides.style.transform = `translateX(-${index * 100}%)`;
+    }
+
+    function startAutoSlide() {
+        autoSlide = setInterval(() => showSlide(index + 1), 3000);
+    }
+
+    function stopAutoSlide() {
+        clearInterval(autoSlide);
+    }
+
+    prevButton.addEventListener('click', () => {
+        stopAutoSlide();
+        showSlide(index - 1);
+        startAutoSlide();
+    });
+
+    nextButton.addEventListener('click', () => {
+        stopAutoSlide();
+        showSlide(index + 1);
+        startAutoSlide();
+    });
+
+    slides.addEventListener('mouseover', stopAutoSlide);
+    slides.addEventListener('mouseout', startAutoSlide);
+
+    startAutoSlide();
